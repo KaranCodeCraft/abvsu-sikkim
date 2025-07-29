086359dfe6a042acee3e6a386bdd9b17412a4d05
dev <dev882624@gmail.com>
2025-07-29 13:51:01 +0530

    erp link

--- app/(aboutsgtu)/(leadership)/vicechancellormessage/page.js
+++ app/(aboutsgtu)/(leadership)/vicechancellormessage/page.js
@@ -28,7 +28,6 @@ const Page = () => {
           <div className="md:col-span-3 space-y-6">
             <h2 className="text-3xl md:text-4xl font-bold leading-tight">
               "Learning is the key that unlocks the door to changing the world."
-               
             </h2>
 
             <p>
--- data/admission.js
+++ data/admission.js
@@ -10,11 +10,18 @@ const AboutData = [
           target: "_blank"
           
         },
-        // {
-        //   name: 'Admission Login',
-        //   link: '/#'
-        // link: 'https://www.oldcode.ABVSU.ac.in/manager/pub/publiclanding'
-        // },
+        {
+          name: 'Employee Login',
+          link: 'https://www.erp.abvsu.ac.in/signin'
+        },
+        {
+          name: 'Faculty Login',
+          link: 'https://www.erp.abvsu.ac.in/centersignin'
+        },
+        {
+          name: 'Student Login',
+          link: 'https://www.erp.abvsu.ac.in/studentsignin'
+        },
         {
           name: 'Academic Calendar',
           link: '/#'
